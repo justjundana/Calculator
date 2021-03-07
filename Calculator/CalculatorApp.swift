4c9b533e4b63ed3d93e8e875bf2d0a2fd5ccc331
Jundana Al-Basyir <jalbasyir@gmail.com>
2021-03-07 18:02:15 +0700

    Calculator SwiftUI

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Jundana Al-Basyir on 07/03/21.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
